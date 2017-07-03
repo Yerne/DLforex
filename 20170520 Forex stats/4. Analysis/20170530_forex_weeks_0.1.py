299e3fb09ce154f22d1b60253c412afa9b04ae22
P.S <espwnwn@gmail.com>
2017-07-03 21:13:13 +0300

    Add files via upload

--- 20170520 Forex stats/4. Analysis/20170530_forex_weeks_0.1.py
+++ 20170520 Forex stats/4. Analysis/20170530_forex_weeks_0.1.py
@@ -201,6 +201,24 @@ df.to_csv('pred_fx_1.csv')
 X1.to_csv('X1.csv')
 y1.to_csv('y1.csv')
 
+
+
+# feature importance 
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(1, 1, figsize=(8, 16))
+xgb.plot_importance(bst_jpy, max_num_features=50, height=0.5, ax=ax)
+
+# Genetic algorithm
+
+from gplearn.genetic import SymbolicRegressor
+est_gp = SymbolicRegressor(population_size=5000,
+                           generations=50, stopping_criteria=0.01,
+                           p_crossover=0.7, p_subtree_mutation=0.1,
+                           p_hoist_mutation=0.05, p_point_mutation=0.1,
+                           max_samples=0.9, verbose=1,
+                           parsimony_coefficient=0.01, random_state=0)
+est_gp.fit(X_all, y_usdjpy)
+est_gp.predict(X2)
 --------------------------------------
 def xgbt(yz):
     dtrain = xgb.DMatrix(X_all, yz),
